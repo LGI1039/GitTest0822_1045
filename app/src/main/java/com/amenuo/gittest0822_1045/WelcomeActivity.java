4980abd6c7eeb65703eb8e2346444c347ea95e65
渲染、流年 <1714534118@qq.com>
2018-08-22 11:31:20 +0800

    1

--- app/src/main/java/com/amenuo/gittest0822_1045/WelcomeActivity.java
+++ app/src/main/java/com/amenuo/gittest0822_1045/WelcomeActivity.java
@@ -2,6 +2,7 @@ package com.amenuo.gittest0822_1045;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        if (true) {
+            Log.i("tag1-","1");
+            Log.i("tag1-","2");
+            Log.i("tag1-","3");
+        }
     }
 }
